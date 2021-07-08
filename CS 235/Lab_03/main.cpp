6b37ea314ab951256d2965c2e1274bd0a4628567
Zarin Loosli <zarinloosli@gmail.com>
2021-07-07 19:56:41 -0600

    Stub main function

--- CS 235/Lab_03/main.cpp
+++ CS 235/Lab_03/main.cpp
@@ -2,6 +2,8 @@
  * Zarin Loosli - 7/9/21
 **/
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <string>
 
 #include "LinkedList.h"
@@ -28,16 +30,85 @@ int main(int argc, char *argv[])
 {
     VS_MEM_CHECK
 
-    LinkedList<string> intList;
-    intList.push_front("Hello");
-    intList.pop_front();
-    intList.front();
-    intList.empty();
-    intList.remove("Hello");
-    intList.clear();
-    intList.reverse();
-    intList.size();
-    intList.toString();
+    // Inputs -------------------------------------------------------------------------------------------------------------
+    // Check number of inputs
+    if (argc < 3)
+    {
+        cerr << "Please provide name of input and output files";
+        return 1;
+    }
+
+    // Try to open input file
+    cout << "Input file: " << argv[1] << endl;
+    std::ifstream in(argv[1]);
+    if (!in)
+    {
+        cerr << "Unable to open " << argv[1] << " for input";
+        return 2;
+    }
+
+    // Try to open output file
+    cout << "Output file: " << argv[2] << endl;
+    std::ofstream out(argv[2]);
+    if (!out)
+    {
+        in.close();
+        cerr << "Unable to open " << argv[2] << " for output";
+        return 3;
+    }
+
+    // Instatiate List ----------------------------------------------------------------------------------------------------
+    LinkedList<string> strList;
+
+    // Process file -------------------------------------------------------------------------------------------------------
+    string currLineString = "";
+    while (getline(in, currLineString)) // Loop through each line of input
+    {
+        // Extract command
+        istringstream currLine(currLineString);
+        string command = "";
+        currLine >> command;
+
+        // Execute command
+        if (command == "Clear")
+        {
+            strList.clear();
+        }
+        else if (command == "Empty")
+        {
+            strList.empty();
+        }
+        else if (command == "Delete")
+        {
+            strList.pop_front();
+        }
+        else if (command == "First")
+        {
+            strList.front();
+        }
+        else if (command == "Insert")
+        {
+            strList.push_front("1");
+        }
+        else if (command == "PrintList")
+        {
+            // FIXME more needed
+            strList.toString();
+        }
+        else if (command == "Remove")
+        {
+            strList.remove("1");
+        }
+        else if (command == "Reverse")
+        {
+            strList.reverse();
+        }
+        else if (command == "Size")
+        {
+            strList.size();
+        }
+    }
+
     return 0;
 }
 
