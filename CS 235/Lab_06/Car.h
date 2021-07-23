ebbb51b4f595d9b4e31d2d050b9e83556b55c41d
Zarin Loosli <zarinloosli@gmail.com>
2021-07-23 16:46:13 -0600

    Update car struct

--- CS 235/Lab_06/Car.h
+++ CS 235/Lab_06/Car.h
@@ -1,15 +1,32 @@
 #ifndef CAR_H_
 #define CAR_H_
 
+#include <sstream>
+#include <iostream>
+
 struct Car
 {
-    /**
-     * Create a car
-     * @param data an unsigned int to store in the car
-     * @return a new Car
-     */
-    Car(unsigned int data = 0) : carData(data) {}
-    unsigned int carData;
+    unsigned int id;
+    Car(unsigned int data = 0) : id(data) {}
+    ~Car() {}
+
+    bool operator==(const Car car) { return this->id == car.id; }
+    bool operator!=(const Car car) { return this->id != car.id; }
+
+    /** Return the Car as a string */
+    std::string toString() const
+    {
+        std::stringstream out;
+        out << id;
+        return out.str();
+    } // end toString()
+
+    /** Make insertion operator friend for a Car */
+    friend std::ostream &operator<<(std::ostream &os, const Car &c)
+    {
+        os << c.toString();
+        return os;
+    } // end operator<<
 };
 
 #endif
