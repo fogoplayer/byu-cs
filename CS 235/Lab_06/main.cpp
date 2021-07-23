93efd1c659f191e20617c3489a41afdb17829a28
Zarin Loosli <zarinloosli@gmail.com>
2021-07-23 16:22:14 -0600

    Update main

--- CS 235/Lab_06/main.cpp
+++ CS 235/Lab_06/main.cpp
@@ -5,7 +5,9 @@
 #include <fstream>
 #include <string>
 
-#include "Deque.h"
+#include "Stack.h"
+#include "Vector.h"
+#include "Queue.h"
 
 using namespace std;
 
@@ -54,36 +56,47 @@ int main(int argc, char *argv[])
 
     // -------------------------------------------------------------------------------------------------------------
     int testArray[] = {1, 2, 3, 4};
-    Deque<int> myDeque(testArray, 4);
-    int testArray1[] = {1, 2, 3};
-    Deque<int> myDeque1(testArray1, 3);
-    int testArray2[] = {1, 2, 3, 4, 5};
-    Deque<int> myDeque2 = Deque<int>(testArray2, 5);
-    Deque<int> emptyDeque;
+    // Deque<int> myDeque(testArray, 4);
+    // int testArray1[] = {1, 2, 3};
+    // Deque<int> myDeque1(testArray1, 3);
+    // int testArray2[] = {1, 2, 3, 4, 5};
+    // Deque<int> myDeque2 = Deque<int>(testArray2, 5);
+    // Deque<int> emptyDeque;
 
-    myDeque.pop_front();
-    myDeque1.push_front(-5);
-    myDeque.push_front(-5);
-    cout << "myDeque " << myDeque << endl;
-    cout << "myDeque1 " << myDeque1 << endl;
-    myDeque.pop_back();
-    myDeque.push_front(-5);
-    myDeque.push_front(-6);
-    myDeque.push_front(-7);
-    cout << myDeque.size() << endl;
-    cout << "myDeque " << myDeque << endl;
-    cout << "myDeque1 " << myDeque1 << endl;
-    cout << "myDeque2 " << myDeque2 << endl;
+    // myDeque.pop_front();
+    // myDeque1.push_front(-5);
+    // myDeque.push_front(-5);
+    // cout << "myDeque " << myDeque << endl;
+    // cout << "myDeque1 " << myDeque1 << endl;
+    // myDeque.pop_back();
+    // myDeque.push_front(-5);
+    // myDeque.push_front(-6);
+    // myDeque.push_front(-7);
+    // cout << myDeque.size() << endl;
+    // cout << "myDeque " << myDeque << endl;
+    // cout << "myDeque1 " << myDeque1 << endl;
+    // cout << "myDeque2 " << myDeque2 << endl;
 
-    cout << emptyDeque.empty() << endl;
-    emptyDeque.push_back(0);
-    cout << emptyDeque.empty() << endl;
-    emptyDeque.push_back(0);
-    cout << emptyDeque.empty() << endl;
-    emptyDeque.pop_back();
-    emptyDeque.pop_back();
-    emptyDeque.push_back(1);
-    cout << emptyDeque.empty() << endl;
+    // cout << emptyDeque.empty() << endl;
+    // emptyDeque.push_back(0);
+    // cout << emptyDeque.empty() << endl;
+    // emptyDeque.push_back(0);
+    // cout << emptyDeque.empty() << endl;
+    // emptyDeque.pop_back();
+    // emptyDeque.pop_back();
+    // emptyDeque.push_back(1);
+    // cout << emptyDeque.empty() << endl;
+
+    Stack<int> myStack;
+    cout << myStack << endl;
+    myStack.push(1);
+    myStack.push(2);
+    myStack.push(3);
+    myStack.push(4);
+    cout << myStack << endl;
+    cout << myStack.size() << endl;
+    myStack.pop();
+    cout << myStack << endl;
 
     return 0;
 }
