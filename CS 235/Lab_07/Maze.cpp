5899a2f612f93b286fca04e8ccfdb49126aa5665
Zarin Loosli <zarinloosli@gmail.com>
2021-07-26 18:52:53 -0600

    Implement stumps and destructor

--- CS 235/Lab_07/Maze.cpp
+++ CS 235/Lab_07/Maze.cpp
@@ -1,30 +1,63 @@
-//**** YOU MAY NOT MODIFY THIS DOCUMENT ****/
-#ifndef MAZE_INTERFACE_H
-#define MAZE_INTERFACE_H
+#ifndef MAZE_H_
+#define MAZE_H_
 #include <string>
 
-class MazeInterface
+class Maze
 {
+private:
+    int rowCount;
+    int colCount;
+    int layerCount;
+    int ***maze;
+
 public:
-    MazeInterface(void) {}
-    ~MazeInterface(void) {}
+    Maze(int ***maze) {}
+    ~Maze(void)
+    {
+        for (size_t i = 0; i < rowCount; i++)
+        {
+            for (size_t i = 0; i < colCount; i++)
+            {
+                for (size_t k = 0; k < layerCount; k++)
+                {
+                    delete maze[i][j][k];
+                }
+            }
 
-    /** Set maze value
-	   @param height
-	   @param width
-	   @param layer
-	   @param value
-	*/
-    virtual void setValue(int height, int width, int layer, int value) = 0;
+            delete[] maze[i][j];
+        }
 
-    /** Solve maze
-	   @return true if solveable, else false
-	*/
-    virtual bool find_maze_path() = 0;
+        delete[] maze[i];
+    }
 
-    /** Output maze (same order as input maze)
-	  @return string of 2D layers
-	*/
-    virtual std::string toString() const = 0;
+    /** 
+     * Set maze value
+	 * @param height
+	 * @param width
+	 * @param layer
+	 * @param value
+	 */
+    virtual void setValue(int height, int width, int layer, int value)
+    {
+        return;
+    }
+
+    /** 
+     * Solve maze
+	 * @return true if solveable, else false
+	 */
+    virtual bool find_maze_path()
+    {
+        return true;
+    }
+
+    /**
+     * Output maze (same order as input maze)
+	 * @return string of 2D layers
+	 */
+    virtual std::string toString() const
+    {
+        return "string";
+    }
 };
-#endif // MAZE_INTERFACE_H
+#endif
