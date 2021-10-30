a906154b68b3579c7ad0940420c5e0404296300f
Zarin Loosli <zarinloosli@gmail.com>
2021-10-29 21:13:20 -0600

    Finish vue

--- 260/learning-vue-cli/src/App.vue
+++ 260/learning-vue-cli/src/App.vue
@@ -1,20 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="header">
+      <h1>Ticket System</h1>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/create">New Ticket</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
     </div>
-    <router-view/>
+    <div class="content">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <style>
+body {
+  margin: 0px;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  background: #571845;
+  padding: 10px 100px;
+  color: white;
 }
 
 #nav {
@@ -23,10 +39,14 @@
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #fff;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #ffc300;
+}
+
+.content {
+  padding: 10px 100px;
 }
 </style>
